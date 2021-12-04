fecaa6dc944685917b5ca7e80391d81ddf4de6ca
Colin Drewes <colindrewes@gmail.com>
2021-12-04 08:25:08 +0000

    Fix makefile

--- designs/ip/cp_const/cp_const.v
+++ designs/ip/cp_const/cp_const.v
@@ -1,17 +1,31 @@
-module coprocessor(
+module cp_const(
     input clk,
     input[31:0] pcpi_rs1,
     input[31:0] pcpi_rs2,
+    input[31:0] pcpi_insn,
+    input pcpi_valid,
     output reg[31:0] pcpi_rd,
-    input trigger,
-    output reg done
+    output reg pcpi_wr,
+    output reg pcpi_ready,
+    output pcpi_wait
 );
 
+assign pcpi_wait = 0;
+reg finish = 0;
+
 always @(posedge clk) begin
-    done <= 0;
-    if (trigger) begin
-        pcpi_rd <= 32'hFFFFFFFF;
-        done <= 1;
+    if (pcpi_valid) begin 
+        pcpi_rd <= pcpi_insn;
+        finish <= 1;
+    end
+    // pcpi wait somewhere in here?
+    if (finish) begin
+        pcpi_ready <= 1;
+        pcpi_wr <= 1;
+        finish <= 0;
+    end else begin
+        pcpi_wr <= 0;
+        pcpi_ready <= 0;
     end
 end
 
